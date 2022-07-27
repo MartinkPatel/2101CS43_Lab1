e182b5d989208fd53a3d2fa7b37dbb3b2ea43361
Martinkpatel <martin_2101cs43@iitp.ac.in>
2022-07-27 18:10:42 +0530

    Addition feature added

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-
+void addition()
+{
+    printf("Enter 1st number : ");
+    int a, b;
+    scanf("%d", &a);
+    printf("Enter 2nd number : ");
+    scanf("%d", &b);
+    printf("The addition of %d and %d is %d\n", a, b, a + b);
+}
+void subtraction() {}
+void division() {}
+void multiplication() {}
 int main()
 {
 
@@ -10,6 +21,31 @@ int main()
     printf("2.Subtraction\n");
     printf("3.Division\n");
     printf("4.Multiplication\n");
+
+    int choice = -1;
+
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+    switch (choice)
+    {
+    case 1:
+        addition();
+        break;
+    case 2:
+        subtraction();
+        break;
+    case 3:
+        division();
+        break;
+    case 4:
+        multiplication();
+        break;
+
+    default:
+        printf("Enter Appropiate choice.\n\n");
+
+        break;
+    }
 
     return 0;
 }
