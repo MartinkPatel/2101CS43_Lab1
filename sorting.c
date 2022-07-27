7a9b3f07ef1bf345874b09bf2568f06dbd5fadd2
Martinkpatel <martin_2101cs43@iitp.ac.in>
2022-07-27 18:39:43 +0530

    Choice menu added

---
+++ sorting.c
@@ -0,0 +1,20 @@
+/*
+AUTHOR- PATEL MARTIN KAMALNAYAN
+ROLL NO -2101CS43
+
+*/
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Select a sorting technique: \n");
+    printf("1.Insertion Sort\n");
+    printf("2.Selection Sort\n");
+    printf("3.Bubble Sort\n");
+    printf("4.Merge Sort\n");
+    printf("5.Quick Sort\n");
+
+    return 0;
+}
