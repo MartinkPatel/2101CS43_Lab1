a5fdf9cc3ccafdf06a236c1c2232cc5b2481a505
Martinkpatel <martin_2101cs43@iitp.ac.in>
2022-07-27 19:22:52 +0530

    merge sort added

--- sorting.c
+++ sorting.c
@@ -57,6 +57,60 @@ void bubble_sort(int a[], int n)
         }
     }
 }
+void merge(int a[], int l, int m, int r)
+{
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = a[i + l];
+    for (int i = 0; i < n2; i++)
+        R[i] = a[i + m + 1];
+
+    int i = 0;
+    int j = 0;
+    int k = l;
+    while (i < n1 && j < n2)
+    {
+
+        if (L[i] <= R[j])
+        {
+            a[k] = L[i];
+            i++;
+        }
+        else
+        {
+            a[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        a[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        a[k] = R[j];
+        j++;
+        k++;
+    }
+}
+void merge_sort(int a[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+        merge_sort(a, l, m);
+        merge_sort(a, m + 1, r);
+        merge(a, l, m, r);
+    }
+}
 int main()
 {
     printf("Select a sorting technique: \n");
@@ -91,6 +145,9 @@ int main()
 
         case 3:
             bubble_sort(a, n);
+            break;
+        case 4:
+            merge_sort(a, 0, n - 1);
             break;
         }
 
