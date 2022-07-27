34429bd85308fe90bd406606f938c78eb8a125fa
Martinkpatel <martin_2101cs43@iitp.ac.in>
2022-07-27 19:35:58 +0530

    quick sort added

--- sorting.c
+++ sorting.c
@@ -111,6 +111,41 @@ void merge_sort(int a[], int l, int r)
         merge(a, l, m, r);
     }
 }
+void swap(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+int partition(int a[], int low, int high)
+{
+    int pivot = a[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++)
+    {
+
+        if (a[j] < pivot)
+        {
+            i++;
+            swap(&a[i], &a[j]);
+        }
+    }
+    swap(&a[i + 1], &a[high]);
+    return (i + 1);
+}
+
+void quickSort(int a[], int low, int high)
+{
+    if (low < high)
+    {
+
+        int pi = partition(a, low, high);
+
+        quickSort(a, low, pi - 1);
+        quickSort(a, pi + 1, high);
+    }
+}
 int main()
 {
     printf("Select a sorting technique: \n");
@@ -148,6 +183,9 @@ int main()
             break;
         case 4:
             merge_sort(a, 0, n - 1);
+            break;
+        case 5:
+            quickSort(a, 0, n - 1);
             break;
         }
 
