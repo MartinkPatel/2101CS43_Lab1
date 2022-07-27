59509b603a9d5a29693524591e05065b18751454
Martinkpatel <martin_2101cs43@iitp.ac.in>
2022-07-27 18:00:18 +0530

    Added Choice Menu

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    printf("Select a operation from the following\n");
+    printf("1.Addition\n");
+    printf("2.Subtraction\n");
+    printf("3.Division\n");
+    printf("4.Multiplication\n");
+
+    return 0;
+}
