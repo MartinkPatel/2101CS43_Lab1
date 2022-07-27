a0b755c6e72d2462a053b3491698110af0eeca88
Martinkpatel <martin_2101cs43@iitp.ac.in>
2022-07-27 18:46:04 +0530

    input of array

--- sorting.c
+++ sorting.c
@@ -6,8 +6,8 @@ ROLL NO -2101CS43
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-
-int main()
+void insertion_sort(int a[], int n) {}
+main()
 {
     printf("Select a sorting technique: \n");
     printf("1.Insertion Sort\n");
@@ -15,6 +15,31 @@ int main()
     printf("3.Bubble Sort\n");
     printf("4.Merge Sort\n");
     printf("5.Quick Sort\n");
+    int choice = -1;
+    printf("Enter your Choice: ");
+    scanf("%d", &choice);
+    if (choice != -1)
+    {
+        printf("Enter size of array: ");
+        int n;
+        scanf("%d", &n);
+        int a[n];
+        printf("Enter elements of array: ");
+        for (int i = 0; i < i; i++)
+        {
+            scanf("%d", &a[i]);
+        }
 
+        switch (choice)
+        {
+        case 1:
+            insertion_sort(a, n);
+            break;
+        }
+    }
+    else
+    {
+        printf("Enter Appropiate chocie.\n\n");
+    }
     return 0;
 }
